244f249ca2e9985ae1203a3cc314cc6016ff01ba
gushiii <1726143054@qq.com>
2022-11-21 14:44:31 +0800

    fix 自调用失效

--- chapter6/src/main/java/com/springboot/chapter6/service/UserService.java
+++ chapter6/src/main/java/com/springboot/chapter6/service/UserService.java
@@ -2,9 +2,13 @@ package com.springboot.chapter6.service;
 
 import com.springboot.chapter6.pojo.User;
 
+import java.util.List;
+
 public interface UserService {
 
     public int insertUser (User user);
     public User getUser (Long id);
+
+    public int insertUsers(List<User> userList);
 
 }
--- chapter6/src/main/java/com/springboot/chapter6/service/impl/UserServiceImpl.java
+++ chapter6/src/main/java/com/springboot/chapter6/service/impl/UserServiceImpl.java
@@ -3,11 +3,16 @@ package com.springboot.chapter6.service.impl;
 import com.springboot.chapter6.dao.UserDao;
 import com.springboot.chapter6.pojo.User;
 import com.springboot.chapter6.service.UserService;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author : GUSHIII
@@ -19,15 +24,29 @@ import org.springframework.transaction.annotation.Transactional;
  * @date : 2022/11/18 15:01
  */
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, ApplicationContextAware {
 
     @Autowired
     UserDao userDao = null;
 
+    private ApplicationContext applicationContext = null;
+
     @Override
-    @Transactional(isolation = Isolation.SERIALIZABLE, timeout = 1,
+    public int insertUsers(List<User> userList) {
+        int count = 0;
+        UserService userService = applicationContext.getBean(UserService.class);
+        for (User user : userList) {
+            count += userService.insertUser(user);
+        }
+        return count;
+    }
+    @Override
+    @Transactional(
+//            isolation = Isolation.SERIALIZABLE, timeout = 1,
 //            propagation = Propagation.REQUIRED
-            propagation = Propagation.NESTED
+//            propagation = Propagation.NESTED
+            isolation = Isolation.READ_COMMITTED, timeout = 1,
+            propagation = Propagation.REQUIRES_NEW
     )
     public int insertUser(User user) {
         return userDao.insertUser(user);
@@ -37,5 +56,10 @@ public class UserServiceImpl implements UserService {
     @Transactional(isolation = Isolation.READ_COMMITTED, timeout = 1)
     public User getUser(Long id) {
         return userDao.getUser(id);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
